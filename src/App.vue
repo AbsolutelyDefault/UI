7046eb9fcfdb64baf1739332b322077946f68eb6
Alexander Kovalchuk <skateray17@gmail.com>
2018-12-12 16:15:08 +0300

    scroll styling

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,19 @@ export default {
   body, html, #app {
     height: 100%;
   }
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    -webkit-border-radius: 10px;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    -webkit-border-radius: 10px;
+    border-radius: 10px;
+    background: #c9c9c9;
+  }
 </style>
